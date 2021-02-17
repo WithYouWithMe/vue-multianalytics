e2bccfce99bc0ae4be63522705d42df45238eff6
benjamin.futterleib <benjamin.futterleib@withyouwithme.com>
2021-02-18 09:22:54 +1100

    remove comments

--- src/modules/ProductTourModule.js
+++ src/modules/ProductTourModule.js
@@ -10,7 +10,6 @@ export default class ProductTourModule extends BasicModule {
   init (initConf = {}) {
 
     (function () {
-      console.log("[INFO] product tour loaded");
       var e = document.createElement("script");
       e.text = `!function(d,w){var t="${initConf.token}",c="chmln",m="identify alias track clear set show on off custom help _data".split(" "),i=d.createElement("script");if(w[c]||(w[c]={}),!w[c].root){w[c].accountToken=t,w[c].location=w.location.href.toString(),w[c].now=new Date;for(var s=0;s<m.length;s++){!function(){var t=w[c][m[s]+"_a"]=[];w[c][m[s]]=function(){t.push(arguments);};}();}i.src="https://fast.trychameleon.com/messo/"+t+"/messo.min.js",i.async=!0,d.head.appendChild(i);}}(document,window);`;
       e.type = "text/javascript";
@@ -23,7 +22,6 @@ export default class ProductTourModule extends BasicModule {
     this.config.debug = initConf.debug
   }
   identify ({ userId, options }) {
-    console.log("[INFO] chameleon identify called");
     chmln.identify(userId, {
       email: options.email,
       created: options.createdAt,
