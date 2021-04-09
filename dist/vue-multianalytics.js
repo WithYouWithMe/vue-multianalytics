892f77fd5018c61123d16912649f7c4f12f6bba3
Benjamin Futterleib <benjamin.futterleib@gmail.com>
2021-04-09 14:27:30 +1000

    Remove log (#18)
    * rm
    
    * rm log
    
    * build the damn thing
    
    Co-authored-by: ben futterleib <benjamin.futterleib@withyouwithme.com>

--- dist/vue-multianalytics.js
+++ dist/vue-multianalytics.js
@@ -3559,7 +3559,6 @@ module.exports =
 
 
 	      (function () {
-	        console.log("[INFO] installing GTM");
 	        var headScript = document.createElement("script");
 	        headScript.text = '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':\n      new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],\n      j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=\n      \'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);\n      })(window,document,\'script\',\'dataLayer\',\'' + initConf.token + '\');';
 	        headScript.type = "text/javascript";
